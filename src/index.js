f8ec622aca727a1c582207eb2fe229fa62efc4a4
AlexPal005 <rudenkoolexandr06@knu.ua>
2024-12-02 16:24:05 +0200

    added connection

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="./manifest.json">
+    <TonConnectUIProvider manifestUrl="https://brown-important-rabbit-172.mypinata.cloud/ipfs/QmbmLu1ZpVioZxXxWc2T2KQQQjTqdBPoxkoA2nsXE8tbcf">
       <BrowserRouter>
         <App />
       </BrowserRouter>
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,12 @@
 import './main.css'
 import { GoRuby } from 'react-icons/go'
-import { useTonConnectUI, useTonWallet } from '@tonconnect/ui-react'
+import { useTonConnectUI } from '@tonconnect/ui-react'
 import { useEffect, useState } from 'react'
 
 export const Main = () => {
   const [TonConnectUI] = useTonConnectUI()
+  const [tonWalletAddress, setTonWalletAddress] = useState('')
   const [isLoading, setIsLoading] = useState(false)
-  //const [tonWalletAddress, setTonWalletAddress] = useState('')
 
   const handleConnect = async () => {
     try {
@@ -19,18 +19,55 @@ export const Main = () => {
     } catch (err) {
       console.log('Error of connection!')
     }
-    console.log(isLoading)
+  }
+
+  const handleConnection = adress => {
+    setTonWalletAddress(adress)
+    setIsLoading(false)
+  }
+
+  const handleDisconnection = () => {
+    setTonWalletAddress('')
+    setIsLoading(false)
   }
 
   useEffect(() => {
-    console.log(TonConnectUI.account?.address)
-  }, [TonConnectUI.account?.address])
+    if (TonConnectUI.account?.address) {
+      handleConnection(TonConnectUI.account?.address)
+    } else {
+      handleDisconnection()
+    }
+
+    const unsubscribe = TonConnectUI.onStatusChange(wallet => {
+      if (wallet) {
+        handleConnection(TonConnectUI.account?.address)
+      } else {
+        handleDisconnection()
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [TonConnectUI])
+
+  //   const formatAddress = address => {
+  //     const tempAddress = Address.parse(address).toString()
+  //     return `${tempAddress.slice(0, 4)}...${tempAddress.slice(-4)}`
+  //   }
+
+  useEffect(() => {
+    console.log(tonWalletAddress)
+  }, [tonWalletAddress])
 
   return (
     <div className="main-page">
-      <button className="button" onClick={handleConnect}>
-        {TonConnectUI.connected ? (
-          <span className="button__title">Connect Wallet</span>
+      <button
+        className={!!tonWalletAddress ? ['button', 'button-err'].join(' ') : ['button', 'button-blue'].join(' ')}
+        onClick={handleConnect}
+      >
+        {!!tonWalletAddress ? (
+          <span className="button__title">Disconnect Wallet</span>
         ) : (
           <>
             <GoRuby className="button__icon" />
